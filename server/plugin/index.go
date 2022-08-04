a79263d505220f7e6ee20bb7998e6afd4eb729b2
David Bainbridge <davidb@waikato.ac.nz>
2022-08-03 22:16:42 -0400

    Currently supressing inclusion of plg_iamge_light as difficulties experienced getting this to compile on host linux distrib (Ubuntu 20)

--- server/plugin/index.go
+++ server/plugin/index.go
@@ -24,7 +24,8 @@ import (
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_editor_onlyoffice"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_console"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_handler_syncthing"
-	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_image_light"
+	// **** Commented out for now, as difficulty getting it to compile with the supporting libtrancode.a library
+	// _ "github.com/mickael-kerjean/filestash/server/plugin/plg_image_light"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_search_stateless"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_scanner"
 	_ "github.com/mickael-kerjean/filestash/server/plugin/plg_security_svg"
